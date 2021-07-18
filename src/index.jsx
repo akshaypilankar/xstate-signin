5f0f38de2b3cf0ff420fe0d4474d46c0c1988912
Akshay Pilankar <37570243+akshaypilankar@users.noreply.github.com>
2021-07-18 10:03:13 +0530

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyles from './globalStyles'
+import SignIn from './SignIn'
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <SignIn />
+  </>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
